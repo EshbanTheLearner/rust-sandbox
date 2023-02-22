754a7e4d68b0261620ad48f015d58d8ec78f2c05
Eshban Suleman <eshban9492@gmail.com>
2023-02-23 01:08:58 +0500

    Defined Main Function

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,3 +4,13 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
+
+fn main() {
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 *scale),
+        height: 50,
+    };
+
+    dbg!(&rect1);
+}
