fb87c737fae938f88991974ba74338c0939eba44
Eshban Suleman <eshban9492@gmail.com>
2023-02-24 01:41:55 +0500

    Added static images

--- catdex/src/main.rs
+++ catdex/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_files::{Files, NamedFile};
+use actix_web::{web, App, HttpServer, Result};
+
+async fn index() -> Result<NamedFile> {
+    Ok(NamedFile::open("./static/index.html")?)
+}
+
+#[actix_main]
+async fn main() -> std::io::Result<()> {
+    println!("Listening on port 8080");
+    HttpServer::new(|| {
+        App::new()
+            .service(
+                Files::new("/static", "static")
+                    .show_files_listening(),
+        )
+        .route("/", web::get().to(index))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
